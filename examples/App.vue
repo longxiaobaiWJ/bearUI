fdda85e22e5a741ac96abd6cae8a4aa0f3d1d24d
崔为俊 <834719201@qq.com>
2020-09-04 18:21:45 +0800

    feat:first commit

---
+++ examples/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <test-button></test-button>
+  </div>
+</template>
+
+<script>
+import TestButton from './TestButton'
+// console.log(TestButton)
+export default {
+  components: {
+    TestButton
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.row {
+  padding: 10px 0;
+}
+.hm-button {
+  margin-left: 10px;
+}
+</style>
