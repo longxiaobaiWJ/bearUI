09e9c7fdf039269e220c53ca33e495c877657b3d
2604150210 <18855032520@163.com>
2020-09-20 19:17:29 +0800

    完成了Progress组件

--- examples/TestProgress.vue
+++ examples/TestProgress.vue
@@ -1,52 +1,52 @@
 <template>
+<div>
   <div>
+    <bu-progress :percentage="50"></bu-progress>
+    <bu-progress :percentage="100" :format="format"></bu-progress>
+    <bu-progress :percentage="100" status="success"></bu-progress>
+    <bu-progress :percentage="100" status="warning"></bu-progress>
+    <bu-progress :percentage="50" status="exception"></bu-progress>
+  </div>
+
+  <div>
+    <bu-progress :text-inside="true" :stroke-width="26" :percentage="70"></bu-progress>
+    <bu-progress :text-inside="true" :stroke-width="24" :percentage="100" status="success"></bu-progress>
+    <bu-progress :text-inside="true" :stroke-width="22" :percentage="80" status="warning"></bu-progress>
+    <bu-progress :text-inside="true" :stroke-width="20" :percentage="50" status="exception"></bu-progress>
+  </div>
+
+  <div>
+    <bu-progress :percentage="percentage" :color="customColor"></bu-progress>
+
+    <bu-progress :percentage="percentage" :color="customColorMethod"></bu-progress>
+
+    <bu-progress :percentage="percentage" :color="customColors"></bu-progress>
     <div>
-      <bu-progress :percentage="50"></bu-progress>
-      <bu-progress :percentage="100" :format="format"></bu-progress>
-      <bu-progress :percentage="100" status="success"></bu-progress>
-      <bu-progress :percentage="100" status="warning"></bu-progress>
-      <bu-progress :percentage="50" status="exception"></bu-progress>
-    </div>
-
-    <div>
-      <bu-progress :text-inside="true" :stroke-width="26" :percentage="70"></bu-progress>
-      <bu-progress :text-inside="true" :stroke-width="24" :percentage="100" status="success"></bu-progress>
-      <bu-progress :text-inside="true" :stroke-width="22" :percentage="80" status="warning"></bu-progress>
-      <bu-progress :text-inside="true" :stroke-width="20" :percentage="50" status="exception"></bu-progress>
-    </div>
-
-    <div>
-      <bu-progress :percentage="percentage" :color="customColor"></bu-progress>
-
-      <bu-progress :percentage="percentage" :color="customColorMethod"></bu-progress>
-
-      <bu-progress :percentage="percentage" :color="customColors"></bu-progress>
       <div>
-        <bu-button-group>
-          <bu-button icon="bu-icon-minus" @click="decrease"></bu-button>
-          <bu-button icon="bu-icon-plus" @click="increase"></bu-button>
-        </bu-button-group>
-      </div>
-    </div>
-
-    <div>
-      <bu-progress type="circle" :percentage="0"></bu-progress>
-      <bu-progress type="circle" :percentage="25"></bu-progress>
-      <bu-progress type="circle" :percentage="100" status="success"></bu-progress>
-      <bu-progress type="circle" :percentage="70" status="warning"></bu-progress>
-      <bu-progress type="circle" :percentage="50" status="exception"></bu-progress>
-    </div>
-
-    <div>
-      <bu-progress type="dashboard" :percentage="percentage" :color="colors"></bu-progress>
-      <div>
-        <bu-button-group>
-          <bu-button icon="bu-icon-minus" @click="decrease"></bu-button>
-          <bu-button icon="bu-icon-plus" @click="increase"></bu-button>
-        </bu-button-group>
+        <bu-button icon="bu-icon-minus" @click="decrease">-</bu-button>
+        <bu-button icon="bu-icon-plus" @click="increase">+</bu-button>
       </div>
     </div>
   </div>
+
+  <div>
+    <bu-progress type="circle" :percentage="0"></bu-progress>
+    <bu-progress type="circle" :percentage="25"></bu-progress>
+    <bu-progress type="circle" :percentage="100" status="success"></bu-progress>
+    <bu-progress type="circle" :percentage="70" status="warning"></bu-progress>
+    <bu-progress type="circle" :percentage="50" status="exception"></bu-progress>
+  </div>
+
+  <div>
+    <bu-progress type="dashboard" :percentage="percentage" :color="customColors"></bu-progress>
+    <div>
+      <div>
+        <bu-button icon="bu-icon-minus" @click="decrease">-</bu-button>
+        <bu-button icon="bu-icon-plus" @click="increase">+</bu-button>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -55,12 +55,26 @@ export default {
     return {
       percentage: 20,
       customColor: '#409eff',
-      customColors: [
-        {color: '#f56c6c', percentage: 20},
-        {color: '#e6a23c', percentage: 40},
-        {color: '#5cb87a', percentage: 60},
-        {color: '#1989fa', percentage: 80},
-        {color: '#6f7ad3', percentage: 100}
+      customColors: [{
+          color: '#f56c6c',
+          percentage: 20
+        },
+        {
+          color: '#e6a23c',
+          percentage: 40
+        },
+        {
+          color: '#5cb87a',
+          percentage: 60
+        },
+        {
+          color: '#1989fa',
+          percentage: 80
+        },
+        {
+          color: '#6f7ad3',
+          percentage: 100
+        }
       ]
     };
   },
@@ -97,6 +111,7 @@ export default {
 .row {
   padding: 10px 0;
 }
+
 .bu-button {
   margin-left: 10px;
 }
--- packages/progress/src/progress.vue
+++ packages/progress/src/progress.vue
@@ -1,43 +1,54 @@
 <template>
-  <div role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" class="bu-progress bu-progress--line">
-    <div class="bu-progress-bar">
-      <div class="bu-progress-bar__outer" style="height: 6px;">
-        <div class="bu-progress-bar__inner" style="width: 50%;">
-          <!---->
-        </div>
+<div role="progressbar" :aria-valuenow="percentage" aria-valuemin="0" aria-valuemax="100" class="bu-progress" :class="['bu-progress--'+type]">
+  <div class="bu-progress-bar" v-if="type === 'line'">
+    <div class="bu-progress-bar__outer" :style="{height: strokeWidth+'px'}">
+      <div class="bu-progress-bar__inner" :style="barStyle">
+        <div class="bu-progress-bar__innerText" v-if="textInside">{{format(percentage)}}</div>
       </div>
     </div>
-    <div class="bu-progress__text" style="font-size: 14.4px;">50%</div>
   </div>
+
+  <div class="bu-progress-circle" :style="{height: width + 'px', width: width + 'px'}" v-else>
+    <svg viewBox="0 0 100 100">
+      <path class="bu-progress-circle__track" :d="trackPath" stroke="#e5e9f2" :stroke-width="relativeStrokeWidth" fill="none" :style="trailPathStyle"></path>
+      <path class="bu-progress-circle__path" :d="trackPath" :stroke="stroke" fill="none" :stroke-linecap="strokeLinecap" :stroke-width="percentage ? relativeStrokeWidth : 0" :style="circlePathStyle"></path>
+    </svg>
+  </div>
+
+  <div class="bu-progress__text" style="font-size: 16px;" v-if="!textInside">{{format(percentage)}}</div>
+</div>
 </template>
 
 <script>
 export default {
   name: 'BuProgress',
   props: {
-    percentage: {
-      type: Number,
-      default: 0
-    },
     type: {
       type: String,
-      default: 'line'
+      default: 'line',
+      validator: val => ['line', 'circle', 'dashboard'].indexOf(val) > -1
+    },
+    percentage: {
+      type: Number,
+      default: 0,
+      required: true,
+      validator: val => val >= 0 && val <= 100
+    },
+    status: {
+      type: String,
+      validator: val => ['success', 'exception', 'warning'].indexOf(val) > -1
     },
     strokeWidth: {
       type: Number,
       default: 6
     },
+    strokeLinecap: {
+      type: String,
+      default: 'round'
+    },
     textInside: {
       type: Boolean,
       default: false
-    },
-    status: {
-      type: String,
-      default: '--'
-    },
-    color: {
-      type: String,
-      default: ''
     },
     width: {
       type: Number,
@@ -47,27 +58,180 @@ export default {
       type: Boolean,
       default: true
     },
-    strokeLinecap: {
-      ype: String,
-      default: 'round'
+    color: {
+      type: [String, Array, Function],
+      default: ''
+    },
+    format: {
+      type: Function,
+      default: percentage => percentage + '%'
     }
   },
+  computed: {
+    barStyle() {
+      const style = {}
+      style.width = this.format(this.percentage)
+      if (!this.color) {
+        let ret
+        switch (this.status) {
+          case 'success':
+            ret = '#13ce66';
+            break;
+          case 'exception':
+            ret = '#ff4949';
+            break;
+          case 'warning':
+            ret = '#e6a23c';
+            break;
+          default:
+            ret = '#20a0ff';
+        }
+        style.backgroundColor = ret
+      } else {
+        style.backgroundColor = this.getCurrentColor(this.percentage)
+      }
+      return style
+    },
+    relativeStrokeWidth() {
+      return (this.strokeWidth / this.width * 100).toFixed(1);
+    },
+    radius() {
+      if (this.type === 'circle' || this.type === 'dashboard') {
+        return parseInt(50 - parseFloat(this.relativeStrokeWidth) / 2, 10);
+      } else {
+        return 0;
+      }
+    },
+    trackPath() {
+      const radius = this.radius;
+      const isDashboard = this.type === 'dashboard';
+      return `
+          M 50 50
+          m 0 ${isDashboard ? '' : '-'}${radius}
+          a ${radius} ${radius} 0 1 1 0 ${isDashboard ? '-' : ''}${radius * 2}
+          a ${radius} ${radius} 0 1 1 0 ${isDashboard ? '' : '-'}${radius * 2}
+          `;
+    },
+    perimeter() {
+      return 2 * Math.PI * this.radius;
+    },
+    rate() {
+      return this.type === 'dashboard' ? 0.75 : 1;
+    },
+    stroke() {
+      let ret;
+      if (this.color) {
+        ret = this.getCurrentColor(this.percentage);
+      } else {
+        switch (this.status) {
+          case 'success':
+            ret = '#13ce66';
+            break;
+          case 'exception':
+            ret = '#ff4949';
+            break;
+          case 'warning':
+            ret = '#e6a23c';
+            break;
+          default:
+            ret = '#20a0ff';
+        }
+      }
+      return ret;
+    },
+    strokeDashoffset() {
+      const offset = -1 * this.perimeter * (1 - this.rate) / 2;
+      return `${offset}px`;
+    },
+    trailPathStyle() {
+      return {
+        strokeDasharray: `${(this.perimeter * this.rate)}px, ${this.perimeter}px`,
+        strokeDashoffset: this.strokeDashoffset
+      };
+    },
+    circlePathStyle() {
+      return {
+        strokeDasharray: `${this.perimeter * this.rate * (this.percentage / 100) }px, ${this.perimeter}px`,
+        strokeDashoffset: this.strokeDashoffset,
+        transition: 'stroke-dasharray 0.6s ease 0s, stroke 0.6s ease'
+      };
+    },
+  },
   methods: {
-    handleClick (e) {
-      this.$emit('click', e)
+    getCurrentColor(percentage) {
+      if (typeof this.color === 'function') {
+        return this.color(percentage);
+      } else if (typeof this.color === 'string') {
+        return this.color;
+      } else {
+        return this.getLevelColor(percentage);
+      }
+    },
+    getLevelColor(percentage) {
+      const colorArray = this.getColorArray().sort((a, b) => a.percentage - b.percentage);
+      for (let i = 0; i < colorArray.length; i++) {
+        if (colorArray[i].percentage > percentage) {
+          return colorArray[i].color;
+        }
+      }
+      return colorArray[colorArray.length - 1].color;
+    },
+    getColorArray() {
+      const color = this.color;
+      const span = 100 / color.length;
+      return color.map((seriesColor, index) => {
+        if (typeof seriesColor === 'string') {
+          return {
+            color: seriesColor,
+            progress: (index + 1) * span
+          };
+        }
+        return seriesColor;
+      });
     }
   }
 }
 </script>
 
-<style lang='scss'>
+<style lang="scss">
 .bu-progress {
   position: relative;
   line-height: 1;
+
   &.bu-progress--line {
     margin-bottom: 15px;
     width: 350px;
   }
+
+  &.bu-progress--circle {
+    margin-right: 15px;
+    display: inline-block;
+
+    .bu-progress__text {
+      position: absolute;
+      top: 50%;
+      left: 0;
+      width: 100%;
+      text-align: center;
+      margin: 0;
+      transform: translateY(-50%);
+    }
+  }
+
+  &.bu-progress--dashboard {
+    display: inline-block;
+
+    .bu-progress__text {
+      position: absolute;
+      top: 50%;
+      left: 0;
+      width: 100%;
+      text-align: center;
+      margin: 0;
+      transform: translateY(-50%);
+    }
+  }
+
   & .bu-progress-bar {
     padding-right: 50px;
     display: inline-block;
@@ -76,7 +240,8 @@ export default {
     margin-right: -55px;
     box-sizing: border-box;
   }
-  & .bu-progress-bar__outer {
+
+  .bu-progress-bar__outer {
     height: 6px;
     border-radius: 100px;
     background-color: #ebeef5;
@@ -84,19 +249,28 @@ export default {
     position: relative;
     vertical-align: middle;
   }
-  & .bu-progress-bar__inner {
+
+  .bu-progress-bar__inner {
     position: absolute;
     left: 0;
     top: 0;
     height: 100%;
-    background-color: #409eff;
     text-align: right;
     border-radius: 100px;
     line-height: 1;
     white-space: nowrap;
     transition: width .6s ease;
   }
-  & .bu-progress__text {
+
+  .bu-progress-bar__innerText {
+    display: inline-block;
+    vertical-align: middle;
+    color: #fff;
+    font-size: 12px;
+    margin: 0 5px;
+  }
+
+  .bu-progress__text {
     font-size: 14px;
     color: #606266;
     display: inline-block;
@@ -105,5 +279,4 @@ export default {
     line-height: 1;
   }
 }
-
 </style>
